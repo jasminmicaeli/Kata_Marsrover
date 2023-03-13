6c2e5475cd32bbdf89f02fdad087b042f9e7e92f
jasmin micaeli <jasmin.micaeli@klosebrothers.de>
2023-03-13 15:57:47 +0100

    create a test to check if our marsrover can move forward

--- src/MarsroverTest.java
+++ src/MarsroverTest.java
@@ -1,7 +1,18 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class MarsroverTest {
 
-    
+    @Test
+    public void beAbleToReturnLastCoordinates_shouldReturn_1_1_N(){
+        String expected = "1:1:N";
+        String actual;
+
+        Marsrover marsrover = new Marsrover();
+        actual = marsrover.getCoordinates();
+
+        assertEquals(expected, actual);
+    }
 
 }
