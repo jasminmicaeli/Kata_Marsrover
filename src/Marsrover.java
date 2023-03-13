b5c56c65af8dd90b5f05d9c745c4785b5ef6d613
jasmin micaeli <jasmin.micaeli@klosebrothers.de>
2023-03-13 16:28:47 +0100

    Add x and y coordinates to be able to track our movements

--- src/Marsrover.java
+++ src/Marsrover.java
@@ -1,6 +1,8 @@
 public class Marsrover {
 
     String latestCoordinates;
+    int x_Coordinate;
+    int y_Coordinate;
 
     public Marsrover(String departureCoordinates) {
         this.latestCoordinates = departureCoordinates;
