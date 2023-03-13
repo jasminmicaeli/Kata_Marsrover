05e994542a58d2b0074c375b081601b5f8c60af2
jasmin micaeli <jasmin.micaeli@klosebrothers.de>
2023-03-13 16:34:49 +0100

    add attribute direction to be able to track our direction

--- src/Marsrover.java
+++ src/Marsrover.java
@@ -3,12 +3,14 @@ public class Marsrover {
     String latestCoordinates;
     int x_Coordinate;
     int y_Coordinate;
+    String direction;
 
     public Marsrover(String departureCoordinates) {
         this.latestCoordinates = departureCoordinates;
         String[] arrOfSingleDatas = departureCoordinates.split(":");
         x_Coordinate = Integer.parseInt(arrOfSingleDatas[0]);
         y_Coordinate = Integer.parseInt(arrOfSingleDatas[1]);
+        direction = arrOfSingleDatas[2];
     }
 
     public String getCoordinates() {
