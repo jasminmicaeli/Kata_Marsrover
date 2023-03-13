cb896d2f2019eb414524682c446ff8c363fc89ef
jasmin micaeli <jasmin.micaeli@klosebrothers.de>
2023-03-13 16:52:37 +0100

    add test to check if we can move forward regardless of direction

--- src/MarsroverTest.java
+++ src/MarsroverTest.java
@@ -1,6 +1,10 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.rmi.MarshalledObject;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -30,6 +34,28 @@ public class MarsroverTest {
 
         assertEquals(expected, actual);
     }
+
+    @ParameterizedTest
+    @MethodSource("movingForwardFacingDifferentDirections")
+    public void beAbleToMoveForward_facingDifferentDirections(String departure, String expected){
+        String actual;
+
+        Marsrover marsrover = new Marsrover(departure);
+        marsrover.move("F");
+        actual = marsrover.getCoordinates();
+
+        assertEquals(expected, actual);
+    }
+
+    private static Stream<Arguments>movingForwardFacingDifferentDirections(){
+        return Stream.of(
+                Arguments.of("3:3:N", "3:4:N"),
+                Arguments.of("3:3:E", "2:3:E"),
+                Arguments.of("3:3:S", "2:3:S"),
+                Arguments.of("3:3:W", "2:3:W")
+        );
+    }
+
 
     @Test
     public void beAbleToMoveBackwards_shouldReturn_2_1_N(){
