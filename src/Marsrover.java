11dc174bbb6eb72524c421f59974227290b98581
jasmin micaeli <jasmin.micaeli@klosebrothers.de>
2023-03-13 16:12:37 +0100

    Add constructor to be able to get coordinates of departure so we know where we start on the planet

--- src/Marsrover.java
+++ src/Marsrover.java
@@ -2,6 +2,10 @@ public class Marsrover {
 
     String latestCoordinates;
 
+    public Marsrover(String departureCoordinates) {
+        this.latestCoordinates = departureCoordinates;
+    }
+
     public String getCoordinates() {
         return latestCoordinates;
     }
